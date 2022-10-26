87bab55747b2724bcabd881209e25ca9512efd39
mininekos <manuel2g2m@gmail.com>
2022-10-26 13:03:46 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PapaComponent } from './papa/papa.component';
+import { HijoComponent } from './hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PapaComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/papa/papa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-papa',
+  templateUrl: './papa.component.html',
+  styleUrls: ['./papa.component.css']
+})
+export class PapaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
