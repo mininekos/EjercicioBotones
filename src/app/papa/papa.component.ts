1b2cbaf09fcf5f43de3f33c4739d1eefeb277c64
mininekos <manuel2g2m@gmail.com>
2022-10-26 13:39:22 +0200

    Terminado

--- src/app/papa/papa.component.ts
+++ src/app/papa/papa.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-papa',
   templateUrl: './papa.component.html',
   styleUrls: ['./papa.component.css']
 })
 export class PapaComponent implements OnInit {
+  texto: String;
 
-  constructor() { }
+  constructor() { 
+    this.texto="";
+  }
 
   ngOnInit(): void {
+  }
+
+  public mandarTexto(cadena:String){
+    return this.texto=cadena;
   }
 
 }
